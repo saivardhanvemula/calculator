a2a3ef1b5f36cba6cca32ca95829a71938c39d9b
saivardhanvemula <saivardhanvemulamncl@gmail.com>
2024-01-08 00:30:58 +0530

    add

--- index.js
+++ index.js
@@ -5,21 +5,29 @@ equation.innerHTML = "";
 result.innerHTML = "";
 // equation.innerHTML=" ";
 // document.querySelector(".display").innerHTML=eq;
-document.querySelectorAll(".num,.op,.pt").forEach((b) => {
+document.querySelectorAll(".num").forEach((b) => {
     b.addEventListener("click", () => {
         // console.log(b.innerHTML);
         x = x + b.innerHTML;
         equation.innerHTML = x;
-        result.innerHTML=eval(x)
+        result.innerHTML = eval(x);
     });
 });
-document.querySelector(".clear").addEventListener("mouseup", () => {
-    x="";
-    equation.innerHTML="";
-    result.innerHTML="";
+document.querySelectorAll(".op").forEach((b) => {
+    b.addEventListener("click", () => {
+        if (! Number(x[x.length - 1])) {
+            x = x + b.innerHTML;
+            equation.innerHTML = x;
+        }
+    });
+});
+document.querySelector(".clear").addEventListener("dblclick", () => {
+    x = "";
+    equation.innerHTML = "";
+    result.innerHTML = "";
 });
 document.querySelector(".equalto").addEventListener("click", () => {
-    x=eval(x)
-    equation.innerHTML=x;
-    result.innerHTML="";
+    x = eval(x);
+    equation.innerHTML = x;
+    result.innerHTML = "";
 });
